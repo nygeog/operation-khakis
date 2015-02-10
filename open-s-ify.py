89dd5962ebac07b0e39328fb5f6789422262aab0
Daniel M Sheehan <daniel.martin.sheehan@gmail.com>
2015-02-10 13:16:37 -0500

    update

--- open-s-ify.py
+++ open-s-ify.py
@@ -7,7 +7,7 @@ from appscript import *
 pause = 10 #this is in seconds, change to how long it'll take for the FREE acount playlist to run out.  
 
 for i in range(1,5): #change this to the number of times you want the loop to run, + 1, 1,5 runs only 4 times. 
-	#theURL = 'http://open.spotify.com/user/nygeog/playlist/3ig96iljYPAV3kbe3WK4ct' #NYgeog playlst
+	#theURL = 'http://open.spotify.com/user/nygeog/playlist/3ig96iljYPAV3kbe3WK4ct' #NYGeog latest mix playlst
 	theURL = 'http://open.spotify.com/user/pilotsuits/playlist/6EyyKO72lOydoNMFEVwjct'  #PilotSuits Playlist. 
 
 	safari = app("Safari")
