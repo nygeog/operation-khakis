7e123a36e2af69fbf2662b6b2c83e9a75405658a
Daniel M Sheehan <daniel.martin.sheehan@gmail.com>
2015-02-10 13:16:12 -0500

    update

--- open-s-ify.py
+++ open-s-ify.py
@@ -1,26 +1,17 @@
+import time, datetime
 import urllib
-#import spotify #install both https://pyspotify.mopidy.com/en/latest/installation/#installing-libspotify    and thedo pyspotify, see the notes at the bottom. 
 import webbrowser
 from appscript import *
 
 ##GOTTA INSTALL appscript https://pypi.python.org/pypi/appscript/
+pause = 10 #this is in seconds, change to how long it'll take for the FREE acount playlist to run out.  
 
-theURL = 'http://open.spotify.com/user/nygeog/playlist/3ig96iljYPAV3kbe3WK4ct'
-theURL = 'http://open.spotify.com/user/pilotsuits/playlist/6EyyKO72lOydoNMFEVwjct'
+for i in range(1,5): #change this to the number of times you want the loop to run, + 1, 1,5 runs only 4 times. 
+	#theURL = 'http://open.spotify.com/user/nygeog/playlist/3ig96iljYPAV3kbe3WK4ct' #NYgeog playlst
+	theURL = 'http://open.spotify.com/user/pilotsuits/playlist/6EyyKO72lOydoNMFEVwjct'  #PilotSuits Playlist. 
 
-#response = urllib.urlopen(theURL)
+	safari = app("Safari")
+	safari.make(new=k.document,with_properties={k.URL:theURL})
 
-
-safari = app("Safari")
-safari.make(new=k.document,with_properties={k.URL:theURL})
-
-# session = spotify.Session()
-# audio = spotify.AlsaSink(session)
-# loop = spotify.EventLoop(session)
-# loop.start()
-# track = session.get_track('spotify:track:3N2UhXZI4Gf64Ku3cCjz2g')
-# track.load()
-# session.player.load(track)
-# session.player.play()
-
-#response.open
+	print 'Loop '+str(i)+' script started at this time: ' + time.strftime('%c') 
+	time.sleep(pause)
