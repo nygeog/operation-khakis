9ee737059cea39beb3c5c68685273de7df10e3e0
Daniel M Sheehan <daniel.martin.sheehan@gmail.com>
2015-02-10 12:50:26 -0500

    udpate

--- open-s-ify.py
+++ open-s-ify.py
@@ -1,17 +1,26 @@
 import urllib
-import spotify #install both https://pyspotify.mopidy.com/en/latest/installation/#installing-libspotify    and thedo pyspotify, see the notes at the bottom. 
+#import spotify #install both https://pyspotify.mopidy.com/en/latest/installation/#installing-libspotify    and thedo pyspotify, see the notes at the bottom. 
+import webbrowser
+from appscript import *
+
+##GOTTA INSTALL appscript https://pypi.python.org/pypi/appscript/
 
 theURL = 'http://open.spotify.com/user/nygeog/playlist/3ig96iljYPAV3kbe3WK4ct'
+theURL = 'http://open.spotify.com/user/pilotsuits/playlist/6EyyKO72lOydoNMFEVwjct'
 
-response = urllib.urlopen(theURL)
+#response = urllib.urlopen(theURL)
 
-session = spotify.Session()
-audio = spotify.AlsaSink(session)
-loop = spotify.EventLoop(session)
-loop.start()
-track = session.get_track('spotify:track:3N2UhXZI4Gf64Ku3cCjz2g')
-track.load()
-session.player.load(track)
-session.player.play()
+
+safari = app("Safari")
+safari.make(new=k.document,with_properties={k.URL:theURL})
+
+# session = spotify.Session()
+# audio = spotify.AlsaSink(session)
+# loop = spotify.EventLoop(session)
+# loop.start()
+# track = session.get_track('spotify:track:3N2UhXZI4Gf64Ku3cCjz2g')
+# track.load()
+# session.player.load(track)
+# session.player.play()
 
 #response.open
